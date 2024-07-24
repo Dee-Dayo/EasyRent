6e568fed63789c40dba4f9406b03ea720fa5f36c
Dayo Akinyemi <darhyor2050@gmail.com>
2024-07-24 12:44:05 +0100

    started landlord controller

--- src/main/java/com/semicolon/EaziRent/controllers/LandlordController.java
+++ src/main/java/com/semicolon/EaziRent/controllers/LandlordController.java
@@ -1,10 +1,19 @@
 package com.semicolon.EaziRent.controllers;
 
+import com.semicolon.EaziRent.dtos.requests.RegisterRequest;
+import com.semicolon.EaziRent.dtos.responses.EaziRentAPIResponse;
+import com.semicolon.EaziRent.dtos.responses.RegisterResponse;
+import com.semicolon.EaziRent.exceptions.EasyRentBaseException;
 import com.semicolon.EaziRent.services.LandlordService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @AllArgsConstructor
@@ -13,6 +22,13 @@ public class LandlordController {
 
     private final LandlordService landlordService;
 
-//    @PostMapping
-//    public
+    @PostMapping("/register")
+    public ResponseEntity<?> registerLandlord(@RequestBody RegisterRequest request) {
+        try {
+            RegisterResponse response = landlordService.register(request);
+            return new ResponseEntity<>(new EaziRentAPIResponse<>(true, response), CREATED);
+        } catch (EasyRentBaseException error){
+            return new ResponseEntity<>(new EaziRentAPIResponse<>(false, error.getMessage()), BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/semicolon/EaziRent/dtos/responses/EaziRentAPIResponse.java
+++ src/main/java/com/semicolon/EaziRent/dtos/responses/EaziRentAPIResponse.java
@@ -1,12 +1,13 @@
 package com.semicolon.EaziRent.dtos.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class EaziRentAPIResponse<T> {
-    private int code;
     private boolean status;
     private T data;
 }
