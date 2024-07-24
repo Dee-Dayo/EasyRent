59f0bf1bd4d8dffb89125b62525e2e762b970df5
Victor-Maverick <victormsonter@gmail.com>
2024-07-24 06:50:43 +0100

    added register

---
+++ src/main/java/com/semicolon/EaziRent/services/EaziBioDataService.java
@@ -0,0 +1,46 @@
+package com.semicolon.EaziRent.services;
+
+import com.semicolon.EaziRent.data.models.BioData;
+import com.semicolon.EaziRent.data.repositories.BioDataRepository;
+import com.semicolon.EaziRent.dtos.requests.RegisterRequest;
+import com.semicolon.EaziRent.dtos.responses.RegisterResponse;
+import com.semicolon.EaziRent.exceptions.EazyRentBaseExceptions;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EaziBioDataService implements BioDataService{
+    private BioDataRepository bioDataRepository;
+    private ModelMapper modelMapper;
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public RegisterResponse register(RegisterRequest request) {
+        validateEmail(request);
+        BioData bioData = modelMapper.map(request, BioData.class);
+        bioData.setPassword(passwordEncoder.encode(bioData.getPassword()));
+        bioDataRepository.save(bioData);
+        RegisterResponse response = modelMapper.map(bioData, RegisterResponse.class);
+        response.setMessage("Successfully registered");
+        return response;
+    }
+
+    private void validateEmail(RegisterRequest request) {
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        String emailRegex2 = "([a-z]\\.)?[a-z]+@(semicolon|enum|learnspace|native.semicolon).africa";
+        if (!request.getEmail().matches(emailRegex) && !request.getEmail().matches(emailRegex2))
+            throw new EazyRentBaseExceptions("Invalid email format");
+        validateExistingEmail(request);
+    }
+
+    private void validateExistingEmail(RegisterRequest request) {
+        List<BioData> bioDataList = bioDataRepository.findAll();
+        bioDataList.forEach(bioData -> {if(bioData.getEmail().equalsIgnoreCase(request.getEmail()))
+        throw new EazyRentBaseExceptions(request.getEmail()+"already exists");});
+    }
+}
--- src/test/java/com/semicolon/EaziRent/services/RenterServiceTest.java
+++ src/test/java/com/semicolon/EaziRent/services/RenterServiceTest.java
@@ -1,10 +1,12 @@
 package com.semicolon.EaziRent.services;
 
+import com.semicolon.EaziRent.dtos.requests.RegisterRequest;
+import com.semicolon.EaziRent.dtos.responses.RegisterResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RenterServiceTest {
@@ -13,6 +15,13 @@ public class RenterServiceTest {
 
     @Test
     public void testRegisterRenter(){
-
+        RegisterRequest request = new RegisterRequest();
+        request.setFirstName("first name");
+        request.setLastName("last name");
+        request.setEmail("test@email.com");
+        request.setPassword("password");
+        RegisterResponse response = renterService.register(request);
+        assertThat(response).isNotNull();
+        assertThat(response.getId()).isNotNull();
     }
 }
