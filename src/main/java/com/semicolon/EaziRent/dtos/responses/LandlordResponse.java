b4a9148fb4a5bf89efecf0792d5346650519815b
Victor-Maverick <victormsonter@gmail.com>
2024-09-12 15:17:22 +0100

    added review apartment

--- src/main/java/com/semicolon/EaziRent/dtos/responses/LandlordResponse.java
+++ src/main/java/com/semicolon/EaziRent/dtos/responses/LandlordResponse.java
@@ -7,11 +7,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class LandlordResponse {
+    private Long id;
     private String firstName;
     private String lastName;
     private String profilePictureUrl;
 
     public LandlordResponse(Landlord landlord) {
+        this.id = landlord.getId();
         firstName = landlord.getBioData().getFirstName();
         lastName = landlord.getBioData().getLastName();
         profilePictureUrl = landlord.getBioData().getMediaUrl();
