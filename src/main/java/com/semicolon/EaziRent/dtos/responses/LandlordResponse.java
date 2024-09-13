b3f06b1b6e6624f90af276fb97aaff998b275cc4
Victor-Maverick <victormsonter@gmail.com>
2024-09-13 13:20:21 +0100

    check

--- src/main/java/com/semicolon/EaziRent/dtos/responses/LandlordResponse.java
+++ src/main/java/com/semicolon/EaziRent/dtos/responses/LandlordResponse.java
@@ -8,12 +8,14 @@ import lombok.Setter;
 @Getter
 public class LandlordResponse {
     private Long id;
+    private String email;
     private String firstName;
     private String lastName;
     private String profilePictureUrl;
 
     public LandlordResponse(Landlord landlord) {
         this.id = landlord.getId();
+        this.email = landlord.getBioData().getEmail();
         firstName = landlord.getBioData().getFirstName();
         lastName = landlord.getBioData().getLastName();
         profilePictureUrl = landlord.getBioData().getMediaUrl();
