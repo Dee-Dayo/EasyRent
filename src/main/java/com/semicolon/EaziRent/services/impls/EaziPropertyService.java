50a8a8804fa4a9382284aa7e28ac724e4a40026c
Victor-Maverick <victormsonter@gmail.com>
2024-10-14 14:33:57 +0100

    commit

--- src/main/java/com/semicolon/EaziRent/services/impls/EaziPropertyService.java
+++ src/main/java/com/semicolon/EaziRent/services/impls/EaziPropertyService.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -66,6 +67,10 @@ public class EaziPropertyService implements PropertyService {
     @Override
     public ViewPropertyResponse findAll() {
         List<Property> properties = propertyRepository.findAll();
+
+        properties = properties.stream()
+                .filter(property -> property.getNoOfApartments() > 0)
+                .toList();
         return getViewPropertyResponse(properties);
     }
 
